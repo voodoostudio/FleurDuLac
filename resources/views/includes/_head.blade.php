9e51fb332e426d3e0caf52b8d57b824e3e837cf1
vadim <cojocaru.vadim@gmail.com>
2017-03-06 17:40:53 +0200

    head

--- resources/views/includes/_head.blade.php
+++ resources/views/includes/_head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <title>La Fleur du Lac - @yield('title')</title>
 <!-- Website's title ( it will be shown in browser's tab ), Change the website's title based on your Hotel information -->
-<meta name="description" content="Colosseum Hotel is a responsive Hotel and Resort HTML template.">
+{{--<meta name="description" content="Colosseum Hotel is a responsive Hotel and Resort HTML template.">--}}
 <!-- Add your Hotel short description -->
 <meta name="keywords" content="Responsive,HTML5,CSS3,XML,JavaScript">
 <!-- Add some Keywords based on your Hotel and your business and separate them by comma -->
