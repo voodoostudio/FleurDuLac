7a012ad3383e9c234279ba54d71ae0a6e882667f
vladimir <vcaraseni@gmail.com>
2018-01-24 15:19:50 +0200

    added new booking blade

---
+++ resources/views/booking-wbe.blade.php
@@ -0,0 +1,21 @@
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Booking</title>
+    <script type='text/javascript' src='https://res-online.ch/wbePlus/incubator.js.php?hotelID=1019&username=WBEPlus&password=ooDSf72M&type=step'></script>
+</head>
+<style>
+    .wbe-header{background-color: #666666;}
+</style>
+<body>
+
+<div id="wbe_container"></div>
+
+</body>
+</html>
